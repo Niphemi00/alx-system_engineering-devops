87b649105d142c84b42e6bd7b00ab193851cc45f
Niphemi00 <134238348+Niphemi00@users.noreply.github.com>
2024-05-07 13:55:21 +0100

    Update 4-repetition_token_3.rb

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 
-require 'oniguruma'
+
 def match_words(args):
-  regex = Oniguruma: :ORegexp.new('hb(t{0,}n)')
+  regex = /hb(t{0,}n)/
   if regex.match(args):
       puts args
   else:
     puts " "
   end
 end
-match_words(ARGS[0])
+match_words(ARGV[0])
